6c812512549fcfdb19b3fa15e94dc1305e18029b
panconpalta <87393512+De0xyS3@users.noreply.github.com>
2023-02-26 01:33:09 -0500

    Update battery_consulting.py

--- PYTHON SCRIPT/SISTEMA DE APAGADO AUTOMATICO UPS  BATERY/SERVER__CHECKER/battery_consulting.py
+++ PYTHON SCRIPT/SISTEMA DE APAGADO AUTOMATICO UPS  BATERY/SERVER__CHECKER/battery_consulting.py
@@ -10,13 +10,22 @@ def shutdown_server():
     # Conexión al servidor remoto
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('192.168.1.23', username='root', password='YourP4ssw0rd')
+
+    while True:
+        try:
+            ssh.connect('192.168.1.23', username='root', password='r0m4n0s.1507')
+            break
+        except:
+            print("No se puede conectar por SSH. Esperando 60 segundos...")
+            time.sleep(60)
 
     # Ejecución del comando para apagar el servidor
     ssh.exec_command('shutdown -h now')
 
     # Cierre de la conexión SSH
     ssh.close()
+
+
 
 # Función para enviar un mensaje por Telegram
 def send_telegram_message(bot_token, chat_id, message):
@@ -44,7 +53,7 @@ def main():
         if response.status_code == 200 and battery_state == '100':
             print('Batería al 50%. Apagando el servidor remoto...')
             shutdown_server()
-            break
+            #break
 
         # Esperar 20 segundos antes de realizar la siguiente comprobación
         time.sleep(20)
