282e7ce3640891e26dcce15b083f80fe3f755466
panconpalta <87393512+De0xyS3@users.noreply.github.com>
2023-03-09 12:50:38 -0500

    Update demo.sh

--- BASH SCRIPT/LFTP SCRIPT Y ENVIAR NOTIFICACION POR CORREO/demo.sh
+++ BASH SCRIPT/LFTP SCRIPT Y ENVIAR NOTIFICACION POR CORREO/demo.sh
@@ -1,25 +1,36 @@
-#!/bin/bash
+import paramiko
+import smtplib
+from email.mime.text import MIMEText
 
 # Configuración de variables
-SFTP_HOST='nombre_del_servidor_remoto'
-SFTP_USERNAME='nombre_de_usuario'
-SFTP_PASSWORD='contraseña_del_usuario'
-SFTP_REMOTE_FILE='/ruta/al/archivo_remoto'
-EMAIL_RECIPIENT='correo_destinatario@example.com'
-EMAIL_SUBJECT='Asunto del correo'
-EMAIL_BODY='Cuerpo del correo'
+SFTP_HOST = 'nombre_del_servidor_remoto'
+SFTP_USERNAME = 'nombre_de_usuario'
+SFTP_PASSWORD = 'contraseña_del_usuario'
+SFTP_REMOTE_FILE = '/ruta/al/archivo_remoto'
+EMAIL_RECIPIENT = 'correo_destinatario@example.com'
+EMAIL_SUBJECT = 'Asunto del correo'
+EMAIL_BODY = 'Cuerpo del correo'
 
-# Descarga del archivo remoto
-sftppass -P $SFTP_PASSWORD sftp $SFTP_USERNAME@$SFTP_HOST << EOF
-  get $SFTP_REMOTE_FILE
-  quit
-EOF
+# Conexión al servidor SFTP
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(SFTP_HOST, username=SFTP_USERNAME, password=SFTP_PASSWORD)
+sftp = ssh.open_sftp()
 
-# Lectura del contenido del archivo
-FILE_CONTENTS=$(cat $(basename $SFTP_REMOTE_FILE))
+# Lectura del contenido del archivo remoto
+remote_file = sftp.open(SFTP_REMOTE_FILE, 'r')
+file_contents = remote_file.read()
+remote_file.close()
 
 # Envío del correo electrónico con el contenido del archivo
-echo "$EMAIL_BODY"$'\n\n'"$FILE_CONTENTS" | mail -s "$EMAIL_SUBJECT" $EMAIL_RECIPIENT
+msg = MIMEText(EMAIL_BODY + '\n\n' + file_contents)
+msg['Subject'] = EMAIL_SUBJECT
+msg['From'] = EMAIL_RECIPIENT
+msg['To'] = EMAIL_RECIPIENT
+s = smtplib.SMTP('localhost')
+s.sendmail(EMAIL_RECIPIENT, [EMAIL_RECIPIENT], msg.as_string())
+s.quit()
 
-# Eliminación del archivo descargado
-rm $(basename $SFTP_REMOTE_FILE)
+# Cierre de la conexión SFTP
+sftp.close()
+ssh.close()
